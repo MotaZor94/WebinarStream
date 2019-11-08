79d683370850061da1f54a992c56b819c90ff5ef
Marko Colic <markocolic@Markos-MacBook-Pro.local>
2019-11-08 21:29:57 +0100

    form validation

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -29,6 +29,18 @@ render() {
 }
 }
 
+const validate = formValues => {
+    const errors = {}
+    if(!formValues.title) {
+        errors.title = 'You must enter a title'
+    }
+    if(!formValues.description) {
+        errors.description = 'Yoiu must enter a description'
+    }
+    return errors
+}
+
 export default reduxForm({
-    form: 'streamCreate'
+    form: 'streamCreate',
+    validate
 })(StreamCreate)
